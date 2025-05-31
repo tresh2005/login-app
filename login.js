98b7b2e2c61456181a7b9833d5b9a6f1f2a7939f
Godstreasure Udoe <134746950+tresh2005@users.noreply.github.com>
2025-05-31 12:09:15 +0530

    Update and rename login.js to Public

--- login.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const LoginSchema = new mongoose.Schema({
-  f_userName: String,
-  f_Pwd: String,
-});
-module.exports = mongoose.model('Login', LoginSchema);
