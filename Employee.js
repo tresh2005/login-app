f9fa23d07407988b97a75d989f122dd51be27681
Godstreasure Udoe <134746950+tresh2005@users.noreply.github.com>
2025-05-31 13:03:55 +0530

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
-const EmployeeSchema = new mongoose.Schema({
-  f_Image: String,
-  f_Name: String,
-  f_Email: { type: String, unique: true },
-  f_Mobile: String,
-  f_Designation: String,
-  f_gender: String,
-  f_Course: [String],
-  f_Createdate: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('Employee', EmployeeSchema);
+const express = require('express');
+const {
+  createEmployee,
+  getEmployees,
+  updateEmployee,
+  deleteEmployee,
+  getEmployeeById,
+} = require('../controllers/employeeController');
+const router = express.Router();
+
+router.get('/', getEmployees);
+router.post('/', createEmployee);
+router.get('/:id', getEmployeeById);
+router.put('/:id', updateEmployee);
+router.delete('/:id', deleteEmployee);
+
+module.exports = router;
